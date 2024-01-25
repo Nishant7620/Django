8ec2ea809e187382062623dbddc0b42385c41380
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-25 19:39:42 +0530

    updated

--- Hero4/Dc/views.py
+++ Hero4/Dc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from Dc.models import Dc
 
 # Create your views here.
 def batman(request):
-    context = {"numbers":[1,2,3,4,5]}
-    return render(request,'Dc/batman.html',context)
+    dc = Dc.objects.all()
+    
+    return render(request,'Dc/batman.html',{"hero":dc})
