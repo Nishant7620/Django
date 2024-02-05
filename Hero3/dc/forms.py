2a55b49bbbe8ee39cf98fc940a18f483ca4ea851
Nishant7620 <nkaranjavkar72@gmail.com>
2024-02-05 19:36:35 +0530

    updated

--- Hero3/dc/forms.py
+++ Hero3/dc/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class Dc(forms.Form):
     name = forms.CharField(label='Full Name')
     heroic_name = forms.CharField()
+    
--- Hero3/dc/views.py
+++ Hero3/dc/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .forms import Dc
+from .forms import Dc
 
 # Create your views here.
 
 def flash(request):
     dc = Dc()
+    if request.method =="POST":
+        m =Dc(request.POST)
+        if m.is_valid():
+            nm = m.cleaned_data['name']
+            hn = m.cleaned_data['heroic_name']
+            print('Name',nm)
+            print('heroic_name',hn)
+            mm = Dc(name = nm,heroic_name = hn)
+            mm.save()
+    else:
+        m = Dc()         
     return render(request,'dc/flash.html',{"dc":dc})
