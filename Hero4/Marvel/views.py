e226afa203761bf7d5c924e6498d535a17a18830
Nishant7620 <nkaranjavkar72@gmail.com>
2024-02-07 22:59:09 +0530

    updated

--- Hero4/Marvel/views.py
+++ Hero4/Marvel/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .forms import MarvelForm
+from .models import Marvel
 
 # Create your views here.
 
 def spiderman(request):
-    s = MarvelForm()
-    return render(request,'Marvel/spiderman.html',{'s':s})
+    # form = MarvelForm()
+    if request.method =="POST":
+        form = MarvelForm(request.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            h = form.cleaned_data['heroic_name']
+            print('Name',n)
+            print('Heroic_name',h)
+            m = Marvel(name = n,heroic_name = h)
+            m.save()
+    else:
+        form = MarvelForm()        
+    return render(request,'Marvel/spiderman.html',{'form':form})
 
     
